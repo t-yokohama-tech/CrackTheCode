08a32aaa3f114430753cd5821de38b894c7db1e7
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-11-27 15:23:08 +0900

    OneNumberCorrectPredicateの実装とテスト追加

--- src/main/java/com/example/demo/OneNumberCorrectPredicate.java
+++ src/main/java/com/example/demo/OneNumberCorrectPredicate.java
@@ -1,2 +1,17 @@
-package com.example.demo;public class OneNumberCorrectPredicate {
+package com.example.demo;
+
+import java.util.function.Predicate;
+
+public class OneNumberCorrectPredicate implements Predicate<Code> {
+
+    @Override
+    public boolean test(Code code) {
+        int trueNum = 1;
+        int trueCount = 0;
+        Code verifyNumber = Code.from(5, 4, 8);
+        for (int i = 0; i < Code.NUM_OF_DIGITS; i++) {
+            if (verifyNumber.getDigit(i) == code.getDigit(i)) trueCount++;
+        }
+        return trueCount == trueNum;
+    }
 }
--- src/test/java/com/example/demo/OneNumberCorrectPredicateTest.java
+++ src/test/java/com/example/demo/OneNumberCorrectPredicateTest.java
@@ -1,2 +1,33 @@
-package com.example.demo;public class OneNumberCorrectPredicateTest {
+package com.example.demo;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OneNumberCorrectPredicateTest {
+
+    private final OneNumberCorrectPredicate target = new OneNumberCorrectPredicate();
+
+    @Nested
+    class test {
+
+        @Test
+        void returnTrue() {
+            var code = Code.from(0, 0, 8);
+
+            var result = target.test(code);
+
+            assertTrue(result);
+        }
+
+        @Test
+        void returnFalse() {
+            var code = Code.from(0, 0, 0);
+
+            var result = target.test(code);
+
+            assertFalse(result);
+        }
+    }
 }
